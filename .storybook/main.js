40718f8eb7fe09754f09c15f90771de984cc3290
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2020-05-23 12:19:10 +0200

    Storyshots done

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+
+
+// Storyshots seems to have some kind of issue with main.ts, so we have been forced to use main.js
+module.exports = {
+    presets: ['@storybook/preset-typescript'],
+    stories: ['../src/**/*.stories.[jt]s?(x)']
+};
--- .storybook/main.ts
+++
@@ -1,5 +0,0 @@
-
-export default {
-    addons: ['@storybook/preset-typescript'],
-    stories: ['../src/**/*.stories.[jt]s?(x)']
-};
